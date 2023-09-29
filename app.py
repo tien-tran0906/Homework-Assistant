f6e4b58caba03b682ec57fc23a7dc66646bc271a
tien-tran0906 <thuytien692002@gmail.com>
2023-09-29 01:15:51 -0700

    modified

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-# Import os to set API key
 import os
-# Import OpenAI as main LLM service
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
-# Bring in streamlit for UI/app interface
 import streamlit as st
-
-# Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store
 from langchain.vectorstores import Chroma
-
-# Import vector store stuff
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
@@ -27,29 +19,18 @@ with st.sidebar:
 
 if api_key:
     # Set APIkey for OpenAI Service
-    # Can sub this out for other LLM providers
     os.environ['OPENAI_API_KEY'] = api_key
-
-
-
 
     # Create instance of OpenAI LLM
     llm = OpenAI(temperature=0.1, verbose=True)
     embeddings = OpenAIEmbeddings()
 
-
-
-
     # Create and load PDF Loader
     loader = PyPDFLoader('Sommerville-Software-Engineering-10ed.pdf')
-    # Split pages from pdf
     pages = loader.load_and_split()
     # Load documents into vector database aka ChromaDB
     store = Chroma.from_documents(pages, embeddings, collection_name='SWE')
 
-
-
-    # Create vectorstore info object - metadata repo?
     vectorstore_info = VectorStoreInfo(
         name="SWE",
         description="a SWE book",
@@ -59,35 +40,24 @@ if api_key:
     toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 
 
-
-
     # Add the toolkit to an end-to-end LC
     agent_executor = create_vectorstore_agent(
         llm=llm,
         toolkit=toolkit,
         verbose=True
     )
-    # Create a text input box for the user
+
+
     prompt = st.text_input('Input your prompt here')
-
-
-
 
     # If the user hits enter
     if prompt:
-        # Then pass the prompt to the LLM
         response = agent_executor.run(prompt)
-        # ...and write it out to the screen
         st.write(response)
 
 
-
-
-        # With a streamlit expander  
         with st.expander('Document Similarity Search'):
-            # Find the relevant pages
             search = store.similarity_search_with_score(prompt)
-            # Write out the first
             st.write(search[0][0].page_content)
 
 
