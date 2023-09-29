a8e5999228331a798e9d25dec6a8037d745a600e
tien-tran0906 <thuytien692002@gmail.com>
2023-09-29 01:00:01 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,97 @@
+# Import os to set API key
+import os
+# Import OpenAI as main LLM service
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+# Bring in streamlit for UI/app interface
+import streamlit as st
+
+# Import PDF document loaders...there's other ones as well!
+from langchain.document_loaders import PyPDFLoader
+# Import chroma as the vector store
+from langchain.vectorstores import Chroma
+
+# Import vector store stuff
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
+
+api = ''
+st.title('🦜🔗 GPT ')
+
+
+with st.sidebar:
+    api_key = st.text_input('Input your OpenAI API Key')
+
+if api_key:
+    # Set APIkey for OpenAI Service
+    # Can sub this out for other LLM providers
+    os.environ['OPENAI_API_KEY'] = api_key
+
+
+
+
+    # Create instance of OpenAI LLM
+    llm = OpenAI(temperature=0.1, verbose=True)
+    embeddings = OpenAIEmbeddings()
+
+
+
+
+    # Create and load PDF Loader
+    loader = PyPDFLoader('Sommerville-Software-Engineering-10ed.pdf')
+    # Split pages from pdf
+    pages = loader.load_and_split()
+    # Load documents into vector database aka ChromaDB
+    store = Chroma.from_documents(pages, embeddings, collection_name='SWE')
+
+
+
+    # Create vectorstore info object - metadata repo?
+    vectorstore_info = VectorStoreInfo(
+        name="SWE",
+        description="a SWE book",
+        vectorstore=store
+    )
+    # Convert the document store into a langchain toolkit
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
+
+
+
+    # Add the toolkit to an end-to-end LC
+    agent_executor = create_vectorstore_agent(
+        llm=llm,
+        toolkit=toolkit,
+        verbose=True
+    )
+    # Create a text input box for the user
+    prompt = st.text_input('Input your prompt here')
+
+
+
+
+    # If the user hits enter
+    if prompt:
+        # Then pass the prompt to the LLM
+        response = agent_executor.run(prompt)
+        # ...and write it out to the screen
+        st.write(response)
+
+
+
+
+        # With a streamlit expander  
+        with st.expander('Document Similarity Search'):
+            # Find the relevant pages
+            search = store.similarity_search_with_score(prompt)
+            # Write out the first
+            st.write(search[0][0].page_content)
+
+
+
+
+
+
